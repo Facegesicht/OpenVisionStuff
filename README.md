3983d7cf983ac0a7ee213d98d61fbe8596b40877
Facegesicht <face.gesicht@gmx.de>
2026-04-23 20:12:21 +0200

    draw boxes around objects

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# OpenVisionStuff" 
+
+https://www.youtube.com/watch?v=fclTFQQvQFQ
+
+dieses video erklärt was du tun musst, um die library zu installieren und dies das 
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+from ultralytics import YOLO
 
+model = YOLO("yolov8n.pt")  # 'n' = nano (fastest)
 
 capture = cv2.VideoCapture(0)
 
@@ -14,15 +16,18 @@ while True:
         print("error mit dem frame")
         break
 
-    cv2.imshow('frame hihi', frame)
+    results = model(frame)
+    r = results[0]
 
-    if cv2.waitKey(0):
+    annotated_frame = results[0].plot()
+
+    cv2.imshow('frame hihi', annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-image = np.zeros((512,512,3), np.uint8)
+#image = np.zeros((512,512,3), np.uint8)
 
-cv2.line(image, (0, 0), (512,512), (255,0,0), 50)
+#cv2.line(image, (0, 0), (512,512), (255,0,0), 50)
 
-cv2.imshow('Das ist das bild', image)
-
-cv2.waitKey(0)
+#cv2.imshow('Das ist das bild', image)
